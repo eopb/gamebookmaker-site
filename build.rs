b7731dcdfa76e88352918a546e0c24889e26d32a
BlueFlame-Fox <joarmstrong2014@gmail.com>
2019-07-18 17:43:26 +0100

    comment out css build script

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("sass")
-        .arg("style:style")
-        .status()
-        .expect("failed to execute sass process");
-    if !output.success() {
-        panic!("Failed to compile css")
-    }
-    println!("{:#?}", output)
+    // let output = Command::new("sass")
+    //     .arg("style:style")
+    //     .status()
+    //     .expect("failed to execute sass process");
+    // if !output.success() {
+    //     panic!("Failed to compile css")
+    // }
+    // println!("{:#?}", output)
 }
