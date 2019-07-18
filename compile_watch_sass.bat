c9415e5ee15df1d8e8883caa6f0741215fc6c067
Ethan Brierley <ethanboxx@gmail.com>
2019-07-18 12:09:59 +0100

    web server

---
+++ compile_watch_sass.bat
@@ -0,0 +1,3 @@
+start cmd /k sass style:style
+timeout 5
+start cmd /k sass --watch style:style
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::{serve::StaticFiles, templates::Template};
+use serde_json::json;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", &json!({}))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/public/style", StaticFiles::from("style"))
+        .attach(Template::fairing())
+        .launch();
 }
