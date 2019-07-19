528475764990be7b5b4615518823aa398cef4a88
Ethan Brierley <ethanboxx@gmail.com>
2019-07-19 22:09:40 +0100

    get post

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::request::Form;
 use rocket_contrib::{serve::StaticFiles, templates::Template};
 use serde_json::json;
 
@@ -16,10 +17,18 @@ fn index() -> Template {
 fn guest_editor() -> Template {
     Template::render("editor", &json!({}))
 }
+#[derive(FromForm)]
+struct Submit {
+    chapter_num_1: String,
+}
+#[post("/guest", data = "<task>")]
+fn guest_editor_post(task: Form<Submit>) -> Template {
+    Template::render("editor", &json!({}))
+}
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, guest_editor])
+        .mount("/", routes![index, guest_editor, guest_editor_post])
         .mount("/public/style", StaticFiles::from("style"))
         .attach(Template::fairing())
         .launch();
