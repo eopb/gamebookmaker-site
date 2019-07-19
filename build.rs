8e7960503145f9b7ac8dfb4718d6c0621075fe94
Ethan Brierley <ethanboxx@gmail.com>
2019-07-19 09:59:38 +0100

    only build sass on linux

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("sass")
-        .arg("style:style")
-        .status()
-        .expect("failed to execute sass process");
-    if !output.success() {
-        panic!("Failed to compile css")
-    }
-    println!("{:#?}", output)
+    if !cfg!(target_os = "windows") {
+        let output = Command::new("sass")
+            .arg("style:style")
+            .status()
+            .expect("failed to execute sass process");
+        if !output.success() {
+            panic!("Failed to compile css")
+        }
+        println!("{:#?}", output)
+    };
 }
