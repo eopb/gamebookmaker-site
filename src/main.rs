0891306a25730f273266c8c68ace693b793f7679
Ethan Brierley <ethanboxx@gmail.com>
2019-07-19 13:39:08 +0100

    guest editor

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,14 @@ fn index() -> Template {
     Template::render("index", &json!({}))
 }
 
+#[get("/guest")]
+fn guest_editor() -> Template {
+    Template::render("editor", &json!({}))
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, guest_editor])
         .mount("/public/style", StaticFiles::from("style"))
         .attach(Template::fairing())
         .launch();
